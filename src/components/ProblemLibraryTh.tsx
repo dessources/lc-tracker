9e0f98b20c91108b8d6dece8afcdda979d143cf4
Jaem Dessources dit Dumesle <jaem.developer@gmail.com>
2026-08-23 12:53:52 -0400

    - Applied special styles to table header when corresponding filter is in use

--- src/components/ProblemLibraryTh.tsx
+++ src/components/ProblemLibraryTh.tsx
@@ -9,11 +9,14 @@ interface Props {
 
 export function ProblemLibraryTh({ sortKey, curSortKey, sortOrder, onClick }: Props) {
 
-
+  const inUse = curSortKey == sortKey
   return (
     <th className="text-left px-4 py-2.5 text-xs text-secondary font-medium" onClick={onClick}>
-      <span className="cursor-pointer hover:underline">{sortKey}</span>
-      <span className="cursor-pointer">
+      <span className={`cursor-pointer ${inUse && 'underline'} hover:underline capitalize`}>
+        {sortKey}
+      </span>
+      <span className={`cursor-pointer ${inUse && 'text-primary'}`}>
+        {" "}
         {(curSortKey != sortKey || sortOrder == 1 ? <span>{"\u25B2"}</span> : <></>)}
         {(curSortKey != sortKey || sortOrder == -1 ? <span>{"\u25BC"}</span> : <></>)}
       </span>
